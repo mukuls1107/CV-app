a89c29b0ebf522c3f8120829716b5354b5e46d50
mukuls1107 <mukulsingh3078@gmail.com>
2024-08-07 12:18:35 +0530

    Change - Intro Page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,31 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import IntroPage from './components/IntroPage';
-import MainPage from './components/MainPage';
+import './styles/App.css'
 
 function App() {
-  // const [ isFirstVisit, setIsFirstVisit ] = useState(true);
-
-  // useEffect(() => {
-  //   const isVisited = localStorage.getItem("is-visited");
-
-  //   if (isVisited)
-  //     setIsFirstVisit(false);
-  // }, []);
-
-  // const getStarted = () => {
-  //   setIsFirstVisit(false);
-  //   localStorage.setItem("is-visited", true);
-  // };
-  return(
-    <div className="App">
-      <IntroPage></IntroPage>
+  
+  return (
+    <div className="intro-container">
+      <div className="intro-content">
+        <div className="code-element">&lt;dev&gt;</div>
+        <h1 className="intro-title">
+          <span className="title-line">Craft Your</span>
+          <span className="title-line">Developer Story</span>
+        </h1>
+        <p className="intro-description">
+          Create a standout resume that speaks the language of tech.
+          Free, powerful, and tailored for developers.
+        </p>
+        <button className="cta-button">Build Your Resume</button>
+        <div className="code-element closing-tag">&lt;/dev&gt;</div>
+      </div>
+      <div className="background-element"></div>
+      <div className="floating-elements">
+        <span className="float-item">{}</span>
+        <span className="float-item">[]</span>
+        <span className="float-item">()</span>
+        <span className="float-item">&lt;/&gt;</span>
+      </div>
     </div>
   )
-  // return( 
-  //   <div className="App">
-  //     { isFirstVisit ? <IntroPage getStartedFn={getStarted} /> : <MainPage /> }
-  //   </div>
-  // );
 }
 
-export default App;
+export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+// import './index.css'
+
+
+let link = document.createElement("link")
+link.href = "https://fonts.googleapis.com/css2?family=Fira+Code&display=swap";
+link.rel = "stylesheet"
+
+document.head.appendChild(link)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
