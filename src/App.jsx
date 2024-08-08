6a5ecb9810fe7580fd473523e2cacc001d3e3034
mukuls1107 <mukulsingh3078@gmail.com>
2024-08-08 11:36:00 +0530

    Fix: Style fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,7 @@ import React, { useState } from "react";
 import IntroPage from "./components/IntroPage";
 
 function App() {
-  // const [showMainPage, setShowMainPage] = useState(false);
-
-  // const handleButtonClick = () => {
-  //   setShowMainPage(true);
-  // };
-
- return (  
-  <IntroPage/>
- )
+  return <IntroPage />;
 }
 
 export default App;
